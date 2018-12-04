741d15cf89c7c39d5f0b3b96eddd6e37fd0e04f9
dashing-frame-3501 <cacamacho91@gmail.com>
2018-12-04 16:49:51 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  reformatted_languages = {}
+  
+  languages.each do |style, languages|
+    languages.each do |language, language_data|
+      #if the language doesn't yet exist add it
+      if reformatted_languages[language] == nil
+        reformatted_languages[language] = {
+          type: language_data[:type],
+          style: [style]
+        }
+      #otherwise add another sryle
+      else
+        reformatted_languages[language][:style] << style
+      end
+    end
+  end
+  reformatted_languages
 end
